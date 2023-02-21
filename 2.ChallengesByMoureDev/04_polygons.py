db09bfc2a286eb82c54208dbab00139ba5ff2b21
EibolDSG <abeldsg@outlook.es>
2023-02-21 13:01:39 +0100

    ChallengesByMoureDev/04_polygons.py

--- 2.ChallengesByMoureDev/04_polygons.py
+++ 2.ChallengesByMoureDev/04_polygons.py
@@ -5,13 +5,19 @@ Challenge Nº4 by MoureDev - POLYGONS
 def area_polygon(polygon, b, h = 0):
 
     if polygon == "Triangle":
-        print("Area = ", (b*h)/2)
+        if h == 0:
+            print("Error, I need h to calculate the area")
+        else:
+            print("Area = ", (b*h)/2)
 
     elif polygon == "Square":
         print("Area = ", b**2)
 
     elif polygon == "Rectangle":
-        print("Area = ", b*h)
+        if h == 0:
+            print("Error, I need h to calculate the area")
+        else:
+            print("Area = ", b*h)
 
 
-area_polygon("Square", 3)
+area_polygon("Rectangle", 3)
